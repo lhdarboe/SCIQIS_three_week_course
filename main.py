27d5b83e3357c179912c2b58cb17a3646bbba50f
Livia Darboe <livia.heick@gmail.com>
2025-08-05 10:40:29 +0200

    fruits are health

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 print('Hello, how is it going? I like bananas!')
 
 print('Hello, its going great! I love bananas too!')
+
+print('Bananas are my favorite fruit!')
+print('Bananas are great for health!')
+print('Bananas are rich in potassium!')
