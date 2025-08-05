410a12aa80ac36e8f8a55824bce8bdeda6fff669
Livia Darboe <livia.heick@gmail.com>
2025-08-05 14:13:23 +0200

    Deletede lot of text

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
 print('Hello, how is it going? I like bananas!')
-
-print('Hello, its going great! I love bananas too!')
-
-print('Bananas are my favorite fruit!')
-print('Bananas are great for health!')
-print('Bananas are rich in potassium!')
-
-
-print('Bananas are a good source of energy! wuhuuuuu')
-print('Bananas can be used in smoothies!')
-print('Bananas can be used in baking!')
-print('Bananas can be eaten raw!')
-print('Bananas can be dried and eaten as snacks!')
